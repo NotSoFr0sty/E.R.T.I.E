266bcecd60fa922d51c756f9dfc209ae944a47af
Tom Pazhayattil <tomharrisp@gmail.com>
2024-02-24 18:42:15 +0530

    created a prototype for Module 2 and saved its output, renamed a test python file

---
+++ ProvingGrounds.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# myArray = np.array([[1,2,3],
+#                     [4,5,6],
+#                     [7,8,9]])
+# print(myArray)
+# print(myArray.shape[0])
+
+i = 0
+for x in range(3):
+    for y in range(3):
+        print(i)
+        i+=1
