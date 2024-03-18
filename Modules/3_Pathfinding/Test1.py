60aab6123d8553283bf254f7490b14851fd49097
Tom Pazhayattil <tomharrisp@gmail.com>
2024-03-18 23:32:37 +0530

    Worked on Module 3 Test1.py

--- Modules/3_Pathfinding/Test1.py
+++ Modules/3_Pathfinding/Test1.py
@@ -43,6 +43,7 @@ class Node:
         self.coords = coords
         self.xCoord = self.coords[0]
         self.yCoord = self.coords[1]
+        self.pixelColor = img[self.xCoord, self.yCoord]
         self.f_cost = 1000000
     
     def __str__(self):
@@ -50,6 +51,17 @@ class Node:
 
     def __lt__(self, other):
         return self.f_cost < other.f_cost
+    
+    def isTraversable(self):
+        '''Returns True if the pixel corresponding to the node is white'''
+
+        if self.pixelColor == 255: return True
+        else: return False
+
+    def setParent(self, parent):
+        self.parent = parent
+
+
 
 def findNodeHavingCoords(coords):
     '''Returns the node in the list "nodes" which has the coordinates "coords." '''
@@ -92,7 +104,7 @@ img = cv.imread('Floorplans/testFP.png', cv.IMREAD_GRAYSCALE)
 rows, cols = img.shape
 
 # initialize
-# create an object for each pixel in the image
+# create a node (object of the Node class) for each pixel in the image
 nodes = []
 for x in range(rows):
     for y in range(cols):
@@ -105,11 +117,12 @@ open = []
 closed = []
 # add the start node to OPEN
 open.append(findNodeHavingCoords(start))
+findNodeHavingCoords(start).setParent(findNodeHavingCoords(start)) # set the start node's parent to itself
 
 pathFound = False
 # core loop
-for i in range(1):
-    # set current to the node in OPEN with the lowest f_cost. Here, sorted will sort by f_cost because __lt__ was manually defined to do so.
+for i in range(1): #TODO: set the proper range/condition
+    # set current to the node in OPEN with the lowest f_cost. Here, sorted() will sort by f_cost because __lt__ was manually defined to do so.
     current = sorted(open)[0]
     # remove current from open
     open.remove(current)
@@ -123,32 +136,24 @@ for i in range(1):
 
     #calculate neighbors list (of current)
     neighbors = getNeighbors(current)
-    for node in neighbors:
-        print(node)
-    # try:
-    #     if (current.coords[0]-1)<0:
-    #         raise IndexError
-    #     top = [current.coords[0]-1, current.coords[1]]
-    #     neighbors.append(findNodeHavingCoords(top))
-    # except IndexError:
-    #     pass
-    # try:
-    #     if (current.coords[0]-1)<0:
-    #         raise IndexError
-    #     topRight = [current.coords[0]-1, current.coords[1]+1]
-    #     neighbors.append(findNodeHavingCoords(topRight))
-    # except IndexError:
-    #     pass
-    # try:
-    #     Right = [current.coords[0], current.coords[1]+1]
-    #     neighbors.append(findNodeHavingCoords(Right))
-    # except IndexError:
-    #     pass
     
     # for each neighbor of the current node...
-    for node in neighbors:
-        pass
+    for neighbor in neighbors:
+        # if neighbor is not traversable or neighbor is in CLOSED
+        if not neighbor.isTraversable() or neighbor in closed:
+            continue # skip to the next neighbor
 
+        # if neighbor is not in OPEN || new path to neighbor is shorter
+        if neighbor not in open or isNewPathShorter(neighbor): #TODO: define this function
+            # set fCost of neighbor
+            neighbor.calcFCost() #TODO: define this function
+
+            # set parent of neighbor to current
+            neighbor.setParent(current)
+
+            # if neighbor is not in OPEN
+            if neighbor not in open:
+                open.append(neighbor) # add neighbor to open
 
 # save test output image
 cv.imwrite('Modules/3_Pathfinding/testOutput.png', img)
