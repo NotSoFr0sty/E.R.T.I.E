082a1448329ebedc7f3367c34c9a23626129c760
Tom Pazhayattil <tomharrisp@gmail.com>
2024-04-09 19:57:03 +0530

    cleaned up the TestWeather app (this app is only used as a reference)

--- TestServer.py
+++ TestServer.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-from weather import getCurrentWeather
+from TestWeather import getCurrentWeather
 from waitress import serve
 
 app = Flask(__name__)
@@ -7,13 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('testIndex.html')
 
 @app.route('/weather')
 def getWeather():
     city = request.args.get('city')
     weatherData = getCurrentWeather(city)
-    return render_template("weather.html", city=city, weatherData=weatherData)
+    return render_template("testWeather.html", city=city, weatherData=weatherData)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
