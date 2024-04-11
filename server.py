5e42e5f823d0ffacf6db49892deecd2cbe84b58c
Tom Pazhayattil <tomharrisp@gmail.com>
2024-04-11 17:57:27 +0530

    updated requirements.txt to include form handling libraries

--- server.py
+++ server.py
@@ -6,12 +6,26 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    # return render_template('index.html') #TODO: use this line after testing render
-    return render_template('render.html')
-    # return app.send_static_file('render.html')
 
-# @app.route('/select-floor-plan')
+    return render_template('index.html')
+    # return render_template('render.html')
 
+@app.route('/select-floor-plan')
+def selectFloorPlan():
+
+    locDict = {
+        "1": "House 1",
+        "2": "House 2",
+        "3": "House 3",
+        "4": "Shopping Mall",
+        "upload": "Custom Floor Plan"
+    }
+    locIndex = request.args.get('location')
+    location = locDict[locIndex]
+    if (locIndex=='upload'):
+        return render_template('upload-floor-plan.html', locIndex=locIndex, location=location)
+    
+    return render_template('select-floor-plan.html', locIndex=locIndex, location=location)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
